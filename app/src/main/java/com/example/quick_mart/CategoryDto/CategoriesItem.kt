9c0aaea5db38c2862b15fb2ee9163b01d711c60f
Yasmine <yasmine.mohamed2626@gmail.com>
2025-10-07 14:49:23 +0300

    Add categories feature

---
+++ app/src/main/java/com/example/quick_mart/CategoryDto/CategoriesItem.kt
@@ -0,0 +1,10 @@
+package com.example.quick_mart.CategoryDto
+
+data class CategoriesItem(
+    val creationAt: String,
+    val id: Int,
+    val image: String,
+    val name: String,
+    val slug: String,
+    val updatedAt: String
+)
