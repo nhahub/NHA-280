6e9f76fa747d99747df6c5b8a011059cd787ca9d
Nour Ahmed <na5359657@gmail.com>
2025-11-18 20:49:48 +0200

    Favorites

--- app/src/main/java/com/example/quick_mart/MainActivity.kt
+++ app/src/main/java/com/example/quick_mart/MainActivity.kt
@@ -1,50 +1,67 @@
 package com.example.quick_mart
-
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.quick_mart.features.categories.ui.CategoriesScreen
-//import com.example.quick_mart.features.home.viewmodel.HomeViewModel
+import com.example.quick_mart.db.LocalDataSourceImp
+import com.example.quick_mart.features.home.repo.HomeRepositoryImp
+import com.example.quick_mart.features.home.viewmodel.HomeViewModel
+import com.example.quick_mart.features.home.viewmodel.HomeViewModelFactory
+import com.example.quick_mart.network.RemoteDataSourceImp
 import com.example.quick_mart.ui.theme.QuickMartTheme1
 
-class MainActivity : ComponentActivity() {
-    //private val viewModel: HomeViewModel by viewModels()
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: HomeViewModel by viewModels{
+        HomeViewModelFactory(HomeRepositoryImp(
+            RemoteDataSourceImp(), LocalDataSourceImp(this)
+        ))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-           QuickMartTheme1 {
-               CategoriesScreen()
-           }
-
-//                @Composable
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .padding(innerPadding),
-//                        contentAlignment = Alignment.Center
-//                    ) {
-//                        //ApiButton(onClick = { viewModel.getAllProducts() })
-//                    }
-//                }
+            QuickMartTheme1 {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        ApiButton(onClick = { viewModel.getResponseAndCache() })
+                    }
+                }
             }
-
-
         }
     }
+}
 
+@Composable
+fun ApiButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "Hit API")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ApiButtonPreview() {
+    QuickMartTheme1 {
+        ApiButton(onClick = {})
+    }
+}
